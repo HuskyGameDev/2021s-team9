c5d350122f67624d2c9133282d59494054208232
Eric Goulet <engoulet@mtu.edu>
2021-02-24 20:41:21 -0500

    Update PauseMenu.cs

--- Saving_Bonnie/Assets/Scripts/PauseMenu.cs
+++ Saving_Bonnie/Assets/Scripts/PauseMenu.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/**
+    This class creates the functionality of the in-game pause menu
+*/
 public class PauseMenu : MonoBehaviour
 {
-    public static bool GameIsPaused = false;
+    public static bool GameIsPaused = false; //true when game is paused
 
-    public GameObject pauseMenuUI;
+    public GameObject pauseMenuUI; //holds the pause menu UI elements
 
-    // Update is called once per frame
+    //Update is called once per frame
+    //checks for the pressing of the esc button to pause/unpause the game
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -24,29 +28,35 @@ public class PauseMenu : MonoBehaviour
         }
     }
 
+    //Resumes the game
     public void Resume ()
     {
+        //remove pause menu UI and unfreeze time
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
     }
 
+    //pauses the game
     void Pause ()
     {
+        //show pause menu UI and freeze time
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
     }
 
+    //returns the player to the main menu
     public void LoadMenu ()
     {
         Resume();
         SceneManager.LoadScene("Main Menu");
     }
 
+    //terminates the game
     public void QuitGame ()
     {
         Debug.Log("Quitting Game...");
-        //Application.Quit();
+        Application.Quit();
     }
 }
