55d4d0a9dd379e9769c4fb12375d3f379bd863c8
Tyler Becker <77254898+tjbecker101@users.noreply.github.com>
2021-03-19 17:37:54 -0400

    Check if Eric messed up the Github

---
+++ Saving_Bonnie/Assets/Tower.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+
+    private GameObject target;
+    public float range = 15f;
+    public float cooldown = 2f;
+    public float firerate = 1f;
+
+    public string enemyTag = "zombie";
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        GameObject[] zombies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestZombie = null;
+        foreach(GameObject zombie in zombies) {
+
+        }
+    }
+}
