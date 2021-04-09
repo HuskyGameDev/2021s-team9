da1c67f515d6472f85b42cb352864e07bac31566
Eric Goulet <engoulet@mtu.edu>
2021-04-08 22:14:11 -0400

    Addition of tutorial page linked to main menu
    describes the basics of the game and how to play

---
+++ Saving_Bonnie/Assets/TutorialButtons.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// This class keeps track of the interaction of the tutorial buttons
+/// </summary>
+public class TutorialButtons : MonoBehaviour
+{
+
+	//takes the user to the menu page
+	public void Menu()
+	{
+		FindObjectOfType<AudioManager>().play("ButtonClick");
+		Debug.Log("Loading"); //Prints to debug screen to ensure button was pressed
+		SceneManager.LoadScene("Main Menu"); //Loads the menu page
+	}
+}
